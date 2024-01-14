9f23465d9b7fc13df77b08b2da246ba01381ac67
Crispy Strawberry <crispy-strawberry102938@outlook.com>
2024-01-15 00:19:59 +0530

    add dependencies

--- damn-core/src/lib.rs
+++ damn-core/src/lib.rs
@@ -1,3 +1,19 @@
+use hickory_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    error::ResolveError,
+    TokioAsyncResolver,
+};
+
+pub struct SrvLookError {}
+
+pub async fn resolve_connection() -> Result<(), ResolveError> {
+    let resolver = TokioAsyncResolver::tokio(ResolverConfig::google(), ResolverOpts::default());
+
+    let srv = resolver.srv_lookup("google.com").await?;
+
+    Ok(())
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
